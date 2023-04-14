890b4c81a6473b4f27d43b83d5e62ef9385155b4
Mike <mckoro@bu.edu>
2023-04-13 21:25:43 -0400

    Adding API call tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const theme = {
+  blue: {
+    default: "#3f51b5",
+    hover: "#283593"
+  },
+  pink: {
+    default: "#e91e63",
+    hover: "#ad1457"
+  }
+};
+
+const Button = styled.button`
+  background-color: ${(props) => theme[props.theme].default};
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: ${(props) => theme[props.theme].hover};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+
+Button.defaultProps = {
+  theme: "blue"
+};
+
+function clickMe() {
+  const default_endpoint = "https://iamkd3trq0.execute-api.us-west-2.amazonaws.com/default/0StampLambda";
+  fetch(default_endpoint, {
+        crossDomain: true,
+        method: 'POST',
+        headers: {
+            // 'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "id": "mike!" })
+    })
+       .then(response => response.json())
+       .then(data => {
+         console.log(data);
+         alert(data);
+       });
+}
 
 function App() {
   return (
@@ -17,6 +72,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+         <Button onClick={clickMe}>Call Lambda</Button>
+       </div>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -11,7 +11,26 @@ root.render(
   </React.StrictMode>
 );
 
+const sendRes = (status, body) => {
+    var response = {
+        statusCode: status,
+        headers: {
+            "Content-Type" : "application/json",
+            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Methods" : "OPTIONS,GET",
+            "Access-Control-Allow-Credentials" : true,
+            "Access-Control-Allow-Origin" : "*",
+            "X-Requested-With" : "*"
+        },
+        body: body
+    };
+    alert(response.statusCode);
+    console.log("response");
+    return response;
+};
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
